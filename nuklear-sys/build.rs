78e7c1e500103035b0eb54cc6b285c39c6c81da4
Varphone Wong <varphone@qq.com>
2019-12-16 14:01:44 +0800

    Add git worktree detection
    Signed-off-by: Varphone Wong <varphone@qq.com>

--- nuklear-sys/build.rs
+++ nuklear-sys/build.rs
@@ -3,12 +3,25 @@ extern crate cc;
 use std::path::Path;
 use std::process::Command;
 
+fn is_git_worktree() -> bool {
+    let cwd = Path::new(".");
+    if Path::new(&cwd.join(".git")).exists() {
+        return true;
+    }
+    if let Some(v) = cwd.parent() {
+        return Path::new(&v.join(".git")).exists();
+    }
+    false
+}
+
 fn main() {
-    if !Path::new("nuklear-c/nuklear/.git").exists() {
-        Command::new("git")
-            .args(&["submodule", "update", "--init", "--recursive"])
-            .status()
-            .unwrap();
+    if is_git_worktree() {
+        if !Path::new("nuklear-c/nuklear/.git").exists() {
+            Command::new("git")
+                .args(&["submodule", "update", "--init", "--recursive"])
+                .status()
+                .unwrap();
+        }
     }
     cc::Build::new()
         .file("nuklear-c/bind.c")
