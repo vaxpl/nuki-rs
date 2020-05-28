7c8a028f52649cc650d9bfb97d17d4f916b6dfa9
Varphone Wong <varphone@qq.com>
2020-05-28 16:29:43 +0800

    impl Debug for Context
    Signed-off-by: Varphone Wong <varphone@qq.com>

--- src/lib.rs
+++ src/lib.rs
@@ -4359,6 +4359,12 @@ pub struct Context {
     internal: nk_context,
 }
 
+impl Debug for Context {
+    fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
+        write!(f, "Context {{}}")
+    }
+}
+
 impl Default for Context {
     fn default() -> Self {
         Context {
